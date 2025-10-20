2efab6397e07e84422b69bb54a9cb17d3d081b58
M4elstr0m <112475171+M4elstr0m@users.noreply.github.com>
2025-10-20 18:25:29 +0200

    Base architecture

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"embed"
+	"go-by-the-roads/backend/app"
 
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
@@ -13,18 +14,18 @@ var assets embed.FS
 
 func main() {
 	// Create an instance of the app structure
-	app := NewApp()
+	app := app.NewApp()
 
 	// Create application with options
 	err := wails.Run(&options.App{
 		Title:  "go-by-the-roads",
 		Width:  1024,
-		Height: 768,
+		Height: 576,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
 		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
-		OnStartup:        app.startup,
+		OnStartup:        app.Startup,
 		Bind: []interface{}{
 			app,
 		},
