5e96ccbefe1a12bf4c5d061dc29b3639ea97ae47
M4elstr0m <112475171+M4elstr0m@users.noreply.github.com>
2025-10-20 21:12:55 +0200

    Added utilitary functions related to SiteSlice

--- backend/roadmaps/maps.go
+++ backend/roadmaps/maps.go
@@ -4,6 +4,6 @@ package roadmaps
 //
 // Structure where are stored all the different tasks the user wants to achieve in ascending order
 type Map struct {
-	Title   string // Title of the whole roadmap
-	Content []Site // An ordered slice containing each user's Site
+	Title    string    // Title of the whole roadmap
+	Elements SiteSlice // An ordered slice containing each user's Site
 }
--- backend/roadmaps/sites.go
+++ backend/roadmaps/sites.go
@@ -1,5 +1,10 @@
 package roadmaps
 
+import (
+	"log"
+	"slices"
+)
+
 // What would be the use of a map if there was no interesting sites to visit?
 //
 // Structure corresponding to a task the user wants to achieve
@@ -9,11 +14,76 @@ type Site struct {
 	// Each Id must be unique within a slice of Site
 	//
 	// The lower the Id is, the sooner the task should be completed
-	Id uint64
+	Id uint
 
-	Title   string // Title of a Site (will be written in bold)
-	Desc    string // Short description of a Site (will be written in regular)
-	Content string // Content of a Site (will be shown on hover)
+	Title       string // Title of a Site (will be written in bold)
+	Description string // Short description of a Site (will be written in regular)
+	Content     string // Content of a Site (will be shown on hover)
 
 	Completed bool // Indicator of completion
+}
+
+// Slice of Site type
+type SiteSlice []Site
+
+func NewSiteFromMap(m Map) Site {
+	return Site{
+		Id: m.Elements.GetLastId() + 1,
+
+		Title:       "New Site",
+		Description: "",
+		Content:     "",
+
+		Completed: false,
+	}
+}
+
+func (ss SiteSlice) IsSorted() bool {
+	if len(ss) >= 2 {
+		for i := 0; i < len(ss)-1; i++ {
+			if ss[i].Id >= ss[i+1].Id {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func (ss *SiteSlice) Sanitize() {
+	// was here
+}
+
+func (ss *SiteSlice) SortById() {
+	if len(*ss) < 2 {
+		log.Println("SortById() was called even if SiteSlice object had a length < 2")
+		return
+	}
+
+	// Check for Id duplicates
+	for i := 1; i < len(*ss); i++ {
+		if (*ss)[i-1].Id == (*ss)[i].Id {
+			ss.Sanitize()
+			break
+		}
+	}
+
+	// Sorts slice
+	slices.SortFunc(*ss, func(a, b Site) int {
+		switch {
+		case a.Id < b.Id:
+			return -1
+		case a.Id > b.Id:
+			return 1
+		default:
+			return 0
+		}
+	})
+
+}
+
+func (ss SiteSlice) GetLastId() uint {
+	if !ss.IsSorted() {
+		ss.SortById()
+	}
+	return ss[len(ss)-1].Id
 }
