cdbf682ac529d3c368c4bf652e50799b6bab556f
M4elstr0m <112475171+M4elstr0m@users.noreply.github.com>
2025-10-21 18:00:06 +0200

    Started SiteSlice logic full rework

--- backend/roadmaps/sites.go
+++ backend/roadmaps/sites.go
@@ -1,12 +1,5 @@
 package roadmaps
 
-import (
-	"go-by-the-roads/backend/settings"
-	"go-by-the-roads/backend/utils"
-	"log"
-	"slices"
-)
-
 // What would be the use of a map if there was no interesting sites to visit?
 //
 // Structure corresponding to a task the user wants to achieve
@@ -28,7 +21,7 @@ type Site struct {
 // Returns a new Site instance depending on a Map
 func NewSiteFromMap(m Map) Site {
 	return Site{
-		Id: m.Elements.GetLastId() + 1,
+		Id: uint(len(m.Elements)),
 
 		Title:       "New Site",
 		Description: "",
@@ -41,56 +34,6 @@ func NewSiteFromMap(m Map) Site {
 // Slice of Site type
 type SiteSlice []Site
 
-// IsSorted reports whether SiteSlice's element's Ids are sorted in ascending order or not
-func (ss SiteSlice) IsSorted() bool {
-	if len(ss) >= 2 {
-		for i := 0; i < len(ss)-1; i++ {
-			if ss[i].Id >= ss[i+1].Id {
-				return false
-			}
-		}
-	}
-	return true
-}
-
-// SortById sorts ss by Site.Id in ascending order
-//
-// It does not allow duplicates
-func (ss *SiteSlice) SortById() {
-	if len(*ss) < 2 {
-		log.Println(utils.WARNING_STR + " [SortById] called even if SiteSlice instance had a len < 2")
-		return
-	}
-
-	slices.SortFunc(*ss, func(a, b Site) int {
-		switch {
-		case a.Id < b.Id:
-			return -1
-		case a.Id > b.Id:
-			return 1
-		default: // Id duplicate handling
-			log.Println(utils.WARNING_STR + " [SortById] Id duplicate found")
-			if settings.AutomaticIdSanitize {
-				tmp := b.Id
-				b.Id = uint(len(*ss)) + 1
-
-				log.Printf("[SortById] Sanitized Site.Id from id=%v into id=%v", tmp, b.Id)
-			}
-			return 0
-		}
-	})
-
-}
-
-// Returns the Id of the last element of ss
-func (ss SiteSlice) GetLastId() uint {
-	if len(ss) == 0 {
-		return 0
-	}
-
-	if !ss.IsSorted() {
-		ss.SortById()
-	}
-
-	return ss[len(ss)-1].Id
+func (ss *SiteSlice) Insert(s Site) {
+	// was here
 }
