4dfea3703f5b9ebfcfcbc43a814182145809635d
M4elstr0m <112475171+M4elstr0m@users.noreply.github.com>
2025-10-25 19:40:21 +0200

    Fixed docstring for utils.go colors

--- backend/utils/utils.go
+++ backend/utils/utils.go
@@ -2,7 +2,11 @@ package utils
 
 import "github.com/fatih/color"
 
-// WARNING in yellow format
+// INFO in green format. For harmless informations
 var INFO_STR string = color.New(color.FgGreen).Sprint("INFO") + " "
+
+// WARNING in yellow format. For non-fatal errors
 var WARNING_STR string = color.New(color.FgYellow).Sprint("WARNING") + " "
+
+// FATAL in red format. For fatal errors
 var FATAL_STR string = color.New(color.FgRed).Sprint("FATAL") + " "
